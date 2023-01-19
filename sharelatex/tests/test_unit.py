0bfd26d1749970fcbca3a709794d926bd7818481
Patrick Stöckle <patrick.stoeckle@posteo.de>
2023-01-19 12:46:14 +0100

    :recycle: Typing_cast

--- sharelatex/tests/test_unit.py
+++ sharelatex/tests/test_unit.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 import tempfile
-import typing
+from typing import cast as typing_cast, Mapping, Sequence, Any, Generator
 import unittest
 from contextlib import contextmanager
 from pathlib import Path
@@ -11,7 +11,7 @@ from sharelatex.cli import RemoteItem, _sync_deleted_items, _sync_remote_files
 
 
 @contextmanager
-def into_tmpdir() -> typing.Generator:
+def into_tmpdir() -> Generator:
     """Run some code in the context of a tmp dir."""
 
     old_cwd = os.getcwd()
@@ -25,13 +25,13 @@ def into_tmpdir() -> typing.Generator:
             os.chdir(old_cwd)
 
 
-def tmpdir(f: typing.Any) -> typing.Any:
+def tmpdir(f: Any) -> Any:
     """
     Temporary directory.
     """
 
-    def _wrapped(*args: typing.Any, **kwargs: typing.Any) -> None:
-        with into_tmpdir() as tmpdir: # type: ignore
+    def _wrapped(*args: Any, **kwargs: Any) -> None:
+        with into_tmpdir() as tmpdir:  # type: ignore
             # create a dummy env there
             f(*args, Path(tmpdir), **kwargs)
 
@@ -42,13 +42,16 @@ class TestPull(unittest.TestCase):
     @patch.object(Path, "rmdir")
     @patch.object(Path, "unlink")
     def test_sync_delete_file_nomore_present_on_server(
-        self, mock_unlink: typing.Any, mock_rmdir: typing.Any
+        self, mock_unlink: Any, mock_rmdir: Any
     ) -> None:
         # simple test one empty folder in the remote server
-        remote_items = typing.cast(typing.Sequence[RemoteItem],[
-            # the rootFolder
-            {"folder_id": "0", "name": ".", "folder_path": ".", "type": "folder"}
-        ])
+        remote_items = typing_cast(
+            Sequence[RemoteItem],
+            [
+                # the rootFolder
+                {"folder_id": "0", "name": ".", "folder_path": ".", "type": "folder"}
+            ],
+        )
         # But one file locally (abs path)
         working_path = Path.cwd()
         f = Path("image.png").resolve()
@@ -60,29 +63,32 @@ class TestPull(unittest.TestCase):
         mock_unlink.assert_called_once()
         mock_unlink.assert_called_with(f)
 
-    @tmpdir # type: ignore
-    def test_sync_remote_files_download_new_files(self, _: typing.Any) -> None:
-        remote_items = typing.cast(typing.Sequence[RemoteItem], [
-            {
-                "folder_id": "rootFolderId",
-                "name": ".",
-                "folder_path": ".",
-                "type": "folder",
-            },
-            {
-                "_id": "myimageId",
-                "folder_id": "0",
-                "name": "myimage.png",
-                "folder_path": ".",
-                "type": "file",
-            },
-        ])
+    @tmpdir  # type: ignore
+    def test_sync_remote_files_download_new_files(self, _: Any) -> None:
+        remote_items = typing_cast(
+            Sequence[RemoteItem],
+            [
+                {
+                    "folder_id": "rootFolderId",
+                    "name": ".",
+                    "folder_path": ".",
+                    "type": "folder",
+                },
+                {
+                    "_id": "myimageId",
+                    "folder_id": "0",
+                    "name": "myimage.png",
+                    "folder_path": ".",
+                    "type": "file",
+                },
+            ],
+        )
         client = MagicMock()
         client.get_file = MagicMock()
         project_id = 0
         working_path = Path.cwd()
         # force to read local OS datetime (not git log datetime)
-        datetimes_dict: typing.Mapping[str, datetime.datetime] = {}
+        datetimes_dict: Mapping[str, datetime.datetime] = {}
         _sync_remote_files(
             client, str(project_id), working_path, remote_items, datetimes_dict
         )
