9998c73acca4e09fdb0e6df53d3c6a7053c70f1a
Patrick Stöckle <patrick.stoeckle@posteo.de>
2023-01-20 12:54:31 +0100

    :fire: Test script

--- sharelatex/tests/test.sh
+++
@@ -1,5 +0,0 @@
-docker cp create-user-with-pass.js  sharelatex:/var/www/sharelatex/web/modules/server-ce-scripts/scripts
-# user creation isn't idempotent
-docker exec --workdir /var/www/sharelatex/web/modules/server-ce-scripts/scripts -i sharelatex node create-user-with-pass.js --email=joe1@inria.fr --pass=TestTest42
-docker exec --workdir /var/www/sharelatex/web/modules/server-ce-scripts/scripts -i sharelatex node create-user-with-pass.js --email=joe3@inria.fr --pass=TestTest42
-docker exec --workdir /var/www/sharelatex/web/modules/server-ce-scripts/scripts -i sharelatex node create-user-with-pass.js --email=joe2@inria.fr --pass=TestTest42
